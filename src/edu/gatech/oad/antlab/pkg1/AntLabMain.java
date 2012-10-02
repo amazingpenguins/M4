6671f283cc369ce17939c22460aaeead2746928b
lizwahler <lizwahler@gmail.com>
2012-10-01 23:21:45 -0300

    Update M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java

--- src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -71,8 +71,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("902411132");
           //Person4 replace P4 with your name
           //and ggg000x with your gt num
-          Person4 p4 = new Person4("P4");
-          toPrint += p4.toString("ggg004x");
+          Person4 p4 = new Person4("Elizabeth Wahler");
+          toPrint += p4.toString("902861904");
 		  //Person5 replace P5 with your name
           //and ggg000x with your gt num
           Person5 p5 = new Person5("Chris Shaw");
